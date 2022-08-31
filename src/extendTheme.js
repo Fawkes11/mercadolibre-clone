f43b6b60058ac8d6af2bf73eab80f227a7b1b160
Hessler Inampuez <hesslerinampuez@gmail.com>
2022-08-31 14:27:33 -0500

    installation and configuration of the chakraUI library

---
+++ src/extendTheme.js
@@ -0,0 +1,20 @@
+import {extendTheme} from '@chakra-ui/react'
+
+const customTheme = extendTheme({
+    colors: {
+        yellow: {
+            500: "#fff159"
+        },
+        blue: {
+            500: "#3483fa",
+            600: "#0864EE",
+            700: "#2968c8",
+            800: "#1259c3"
+        },
+        green: {
+            
+        }
+    }
+})
+
+export default customTheme
