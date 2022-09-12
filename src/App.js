faebd4863c0949891197389fe5f57edbf7c6e8cf
Hessler Inampuez <hesslernevin@gmail.com>
2022-09-12 10:40:27 -0500

    Route configuration in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Login from './components/views/login'
+import Error404 from './components/views/error404'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Login/>
-    </>
+    <Routes>
+      <Route path="/" element={<Login />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="*" element={<Error404 />}/>
+    </Routes>
   );
 }
 
