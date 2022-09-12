acbe328d1ef7a3b7cc006fabeec2f8b0a1ac6f78
Hessler Inampuez <hesslernevin@gmail.com>
2022-09-12 00:51:59 -0500

    finished login component, with option to modify inputs for reuse

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,53 +1,111 @@
-import React from 'react'
-import { Container, Box, Link, FormControl, FormLabel, Input, FormHelperText, VStack, Heading } from '@chakra-ui/react'
+import React from "react";
+import {
+    Divider,
+    Text,
+    Box,
+    Link,
+    FormControl,
+    FormLabel,
+    Input,
+    FormHelperText,
+    VStack,
+    Flex,
+    Button,
+    Container,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
 
 const Login = () => {
     return (
-
-        <Box w='100%'>
-            <Box
-                w='100%'
-                h='48px'
-                bg='yellow.500'
-                pos='relative'
-            >
-                <Link
-                    pos="absolute"
-                    top="8px"
-                    left="10px"
-                    w='44px'
-                    h='32px'
-                    href='#'
-                    bgImage="url('https://http2.mlstatic.com/frontend-assets/ui-navigation/5.18.1/mercadolibre/logo__small@2x.png')"
-                    bgRepeat="no-repeat"
-                    bgSize="44px 31px"
-                >
-
-                </Link>
+        <Flex
+            w="100%"
+            bg="blackAlpha.200"
+            h="100vh"
+            spacing={0}
+            direction="column"
+            alignItems="center"
+        >
+            <Box w="100%" h={{ base: "48px", md: "56px" }} bg="yellow.500">
+                <Container maxW="1200px" h="100%" pos="relative">
+                    <Link
+                        pos="absolute"
+                        top={{ base: "8px", lg: "11px" }}
+                        left="10px"
+                        w={{ base: "44px", lg: "134px" }}
+                        h={{ base: "32px", lg: "34" }}
+                        href="#"
+                        bgImage={{
+                            base: "url('https://http2.mlstatic.com/frontend-assets/ui-navigation/5.18.1/mercadolibre/logo__small@2x.png')",
+                            lg: "https://http2.mlstatic.com/frontend-assets/ui-navigation/5.18.1/mercadolibre/logo__large_plus@2x.png",
+                        }}
+                        bgRepeat="no-repeat"
+                        bgSize={{ base: "44px 32px", lg: "134px 34px" }}
+                    ></Link>
+                </Container>
             </Box>
-            <VStack px={['24px', '64px']} bg='red.500' spacing={6}>
-                <Heading as='h1' size='sm' noOfLines={2}>
+            <Box w="100%" h={{ base: "0px", md: "182px" }} bg="yellow.500"></Box>
+
+            <VStack
+                bg="white"
+                pos="relative"
+                w={{ base: "100%", md: "424px" }}
+                px={{ base: "24px", md: "64px" }}
+                pt={{ base: "0", md: "32px" }}
+                h={{ base: "100%", md: "auto" }}
+                mt={{ base: "0", md: "-150px" }}
+                borderRadius="6px"
+            >
+                <Text
+                    alignSelf="start"
+                    as="h1"
+                    fontWeight="semibold"
+                    fontSize={{ base: "xl", md: "2xl" }}
+                    size="sm"
+                    noOfLines={2}
+                    mt={{ base: "30px", md: "16px" }}
+                >
                     ¡Hola! Ingresa tu teléfono, e‑mail o usuario
-                </Heading>
-                <form>
-                    <FormControl as='fieldset'>
-                        <FormLabel as='legend'>Email address</FormLabel>
-                        <Input type='email' />
-                        <FormHelperText>We'll never share your email.</FormHelperText>
+                </Text>
+
+                <VStack as="form" spacing={4} w="100%">
+                    <FormControl as="fieldset">
+                        <FormLabel as="legend" fontSize="sm" fontWeight="normal">
+                            Teléfono, E-mail o usuario
+                        </FormLabel>
+                        <Input name="email" type="email" h="48px" />
+                        {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
                     </FormControl>
-                    <FormControl as='fieldset'>
-                        <FormLabel as='legend'>Password</FormLabel>
-                        <Input type='password' />
-                        <FormHelperText>We'll never share your email.</FormHelperText>
+                    <FormControl as="fieldset">
+                        <FormLabel as="legend" fontSize="sm" fontWeight="normal">
+                            Contraseña
+                        </FormLabel>
+                        <Input name="password" type="password" h="48px" />
+                        {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
                     </FormControl>
-                </form>
-                <Link href='#' color='blue.500' p='16px' bg='whiteBack.500'>Necesito ayuda para ingresar</Link>
+                    <Button w="100%" colorScheme="blue" h="48px">
+                        Continuar
+                    </Button>
+                    <Button w="100%" colorScheme="white" textColor="blue.500" h="48px">
+                        Crear cuenta
+                    </Button>
+                </VStack>
+
+                <Divider w={{ base: "0", md: "424px" }} />
+                <Button
+                    mt={0}
+                    pos="relative"
+                    bottom="0"
+                    fontSize="sm"
+                    w={{ base: "auto", md: "424px" }}
+                    colorScheme="white"
+                    textColor="blue.500"
+                    h="48px"
+                >
+                    Necesito ayuda para ingresar
+                </Button>
             </VStack>
+        </Flex>
+    );
+};
 
-
-        </Box>
-
-    )
-}
-
-export default Login
+export default Login;
