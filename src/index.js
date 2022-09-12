134ceb1dbbc5973c31b1e0c8db21ba343ecab200
Hessler Inampuez <hesslernevin@gmail.com>
2022-09-12 10:12:46 -0500

    browserRouter configuration

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import customTheme from './extendTheme'
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider resetCSS theme={customTheme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
