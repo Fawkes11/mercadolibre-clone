69f10802b637ae99e7f77038bfce6ce57c936df6
Hessler Inampuez <hesslernevin@gmail.com>
2022-09-28 10:39:17 -0500

    route corrections

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider resetCSS theme={customTheme}>
-      <BrowserRouter>
+      <BrowserRouter basename='mercadolibre-clone/'>
         <App />
       </BrowserRouter>
     </ChakraProvider>
